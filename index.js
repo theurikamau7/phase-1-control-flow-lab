f7c4098a9aab5e667a27589a7d9a65881a698987
Iankamau1 <theurikamau7@gmail.com>
2023-07-05 22:00:18 +0300

    completed control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+if (ride===199){
+  return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ else if(ride===2001){
+  
+  return 'I will gladly take your thirty bucks.'
+
 }
 
-function switchOnCharmFromTip(){
+ else (ride===2501)
+  return 'No can do.'
+}
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+ 
+  return city === 'NYC'? "Ok, sounds good." : "No go." 
+
+}
+ternaryCheckCity(true)
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous': 
+      return "Thank you."
+      break;
+    default: 'if anything else'
+    return "Bye."
+  }
+
 }
